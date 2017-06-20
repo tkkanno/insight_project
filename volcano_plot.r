55e67dfde37f7c260a0996bbf4bbc7a841ad2745
Tokuwa Kanno <tkkanno@gmail.com>
2017-06-20 12:33:48 +0100

    Update volcano_plot.r

--- volcano_plot.r
+++ volcano_plot.r
@@ -34,7 +34,7 @@ volcano.plot<-function(df, log.fold.change = TRUE) {
   geom_hline(yintercept = -log(0.05), linetype = 2) + 
   geom_vline(xintercept = 1, linetype =2) +
   geom_vline(xintercept = -1, linetype =2) +
-  labs(x = "Fold Change", y = "-log(FRD Adjusted p-value)") +
+  labs(x = "Fold Change", y = "-log(FDR Adjusted p-value)") +
   theme(axis.text =     element_text(size = rel(1)),
 	axis.title.x =  element_text(size = rel(1.25)), 
 	axis.title.y =  element_text(size = rel(1.25)),
@@ -61,7 +61,7 @@ volcano.plot<-function(df, log.fold.change = TRUE) {
   geom_hline(yintercept = -log(0.05), linetype = 2) + 
   geom_vline(xintercept = 1, linetype =2) +
   geom_vline(xintercept = -1, linetype =2) +
-  labs(x = "log2(Fold Change)", y = "-log(FRD Adjusted p-value)") +
+  labs(x = "log2(Fold Change)", y = "-log(FDR Adjusted p-value)") +
   theme(axis.text =     element_text(size = rel(1)),
 	axis.title.x =  element_text(size = rel(1.25)), 
 	axis.title.y =  element_text(size = rel(1.25)),
